4160a4e83a2485cb521658e5e62163673e20b6b9
Mohammad Khalid Kassem Agha <mkhalid.k.agha@gmail.com>
2021-12-12 13:40:52 +0200

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,14 @@
+### Basic Queries
+1. SELECT * from students
+
+2.select * from students where Age > 30
+
+3.select * from students where Gender = 'F' AND age =30
+
+4.SELECT students.name ,students.Points  from students where students.name= 'Alex';
+
+5.INSERT INTO students VALUES (7 ,'Khalid' , 21 , 'M' , 500)
+
+6.UPDATE students SET Points = 550 WHERE students.name="Basma"
+
+7.UPDATE students SET Points = 150 WHERE students.name="Alex"
